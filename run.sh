60698f932b8cc70aec0440108c2e0287ce3cfd15
Andrew Cain <macite@gmail.com>
2023-09-14 04:44:53 +0000

    add c# initial details

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cd /workspaces/syntax-diagram-generator/src
 python3 eebnf_exporter.py ../syntax ../out y
--- src/build-file.sh
+++ src/build-file.sh
@@ -3,7 +3,9 @@
 cd $1
 pdflatex -shell-escape -interaction=batchmode $2
 
+pdftoppm -png "${2%.*}.pdf" > "${2%.*}.png"
+
 rm -f *.aux
 rm -f *.log
 rm -f *.pdf
-# rm -f $2
+rm -f $2
